175dc5e62322c28aa176943f04fbe68b3f46e35c
Zain Malik <zainalimalik7676@gmail.com>
2024-08-06 12:30:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const butterflies = document.querySelectorAll('.butterfly');
+    const butterfly = document.querySelector('.butterfly');
+    let butterflyX = window.innerWidth / 2;
+    let butterflyY = window.innerHeight / 2;
+    let butterflySpeed = 5;
 
-    function createParticle(x, y) {
-        const particle = document.createElement('div');
-        particle.classList.add('particle');
-        particle.style.left = `${x}px`;
-        particle.style.top = `${y}px`;
-        document.body.appendChild(particle);
-
-        setTimeout(() => {
-            particle.remove();
-        }, 2000);
+    function updateButterflyPosition() {
+        butterfly.style.left = `${butterflyX}px`;
+        butterfly.style.top = `${butterflyY}px`;
     }
 
-    butterflies.forEach(butterfly => {
-        butterfly.addEventListener('animationiteration', () => {
-            const rect = butterfly.getBoundingClientRect();
-            createParticle(rect.left + rect.width / 2, rect.top + rect.height / 2);
-        });
-    });
+    function moveButterfly(e) {
+        const { clientX: x, clientY: y } = e;
+        const rect = butterfly.getBoundingClientRect();
+        const centerX = x - rect.width / 2;
+        const centerY = y - rect.height / 2;
+
+        if (centerX > 0 && centerX < window.innerWidth - rect.width) {
+            butterflyX = centerX;
+        }
+        if (centerY > 0 && centerY < window.innerHeight - rect.height) {
+            butterflyY = centerY;
+        }
+
+        updateButterflyPosition();
+    }
+
+    document.addEventListener('mousemove', moveButterfly);
+    updateButterflyPosition();
 });
